659794ec3fd09cf444534107299ef3ad9b0b69e9
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2022-11-22 09:39:22 +0100

    alumno creado

---
+++ src/Alumno.java
@@ -0,0 +1,55 @@
+package src;
+
+public class Alumno {
+
+    String nombre, apellido1, apellido2, centro;
+    Integer dni;
+
+    public Alumno(Integer DNI, String centro, String nombre, String apellido1, String apellido2){
+        this.nombre= nombre;
+        this.dni=DNI;
+        this.centro = centro;
+        this.apellido1=apellido1;
+        this.apellido2=apellido2;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public String getCentro() {
+        return centro;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public void setCentro(String centro) {
+        this.centro = centro;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
