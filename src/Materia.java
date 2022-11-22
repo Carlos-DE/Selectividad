1be1ad479303be262a110e80edcfbbda73367b99
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2022-11-22 09:44:05 +0100

    materia creada

---
+++ src/Materia.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Materia{
+
+    String IdMateria;
+
+    public Materia(String IdMateria){
+        this.IdMateria= IdMateria;
+    }
+
+    public String getIdMateria() {
+        return IdMateria;
+    }
+
+    public void setIdMateria(String idMateria) {
+        this.IdMateria = idMateria;
+    }
+}
