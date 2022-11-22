c65ff687c4769ceb9f8a52545f6b6c1e28e5498c
Pablo <pvpcmg4@uma.es>
2022-11-22 10:11:18 +0100

    Sede y responsable añadido

---
+++ src/Responsable.java
@@ -0,0 +1,47 @@
+package src;
+
+public class Responsable{
+
+    Integer idResponsable;
+
+    String nombre, apellido1, apellido2;
+
+    public Responsable(Integer idResponsable, String nombre, String apellido1, String apellido2){
+        this.idResponsable = idResponsable;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+
+    public Integer getIdResponsable() {
+        return idResponsable;
+    }
+
+    public void setIdResponsable(Integer idResponsable) {
+        this.idResponsable = idResponsable;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+}
---
+++ src/Sede.java
@@ -0,0 +1,28 @@
+package src;
+
+public class Sede{
+
+    Integer idSede;
+    String nombre;
+
+    public Sede(Integer idSede, String nombre) {
+        this.idSede = idSede;
+        this.nombre = nombre;
+    }
+
+    public Integer getIdSede() {
+        return idSede;
+    }
+
+    public void setIdSede(Integer idSede) {
+        this.idSede = idSede;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
