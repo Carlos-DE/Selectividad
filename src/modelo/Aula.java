ec72f86a881b83a81babe7538109d5c42bd3a228
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2022-12-19 20:09:08 +0100

    aula con aforo 2

--- src/modelo/Aula.java
+++ src/modelo/Aula.java
@@ -3,12 +3,17 @@ package modelo;
 public class Aula {
 
 	String id;
+	int aforo;
 	
-	public Aula(String id) {
+	public Aula(String id, int aforo) {
 		this.id = id;
+		this.aforo = aforo;
 	}
 	
 	public String getId() {
 		return id;
+	}
+	public int getAforo() {
+		return aforo;
 	}
 }
