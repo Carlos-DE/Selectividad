42db1098eb89d4cef53d9dd87427bb908dae254d
sergioM <sermorepe@gmail.com>
2022-12-20 14:22:15 +0100

    fix bug y reodernar codigo

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -6,64 +6,62 @@ public class Controlador{
 
 	ConexionConBaseDeDatos conexionBD;
 	private VistaHome vistaHome;
-	private VistaSedes vistaSedes;
-	private VistaAsignaturas vistaAsignaturas;
 	private VistaAlumnos vistaAlumnos;
-	private VistaResponsablesSede vistaResponsables;
-	private VistaResponsablesExamen vistaResponsablesExamen;
+	private VistaAsignaturas vistaAsignaturas;
 	private VistaExamen vistaExamen;
-	private VistaAula vistaAula;
 	private VistaInstituto vistaInstituto;
+	private VistaSedes vistaSedes;
+	private VistaResponsablesSede vistaResponsables;
+	
+	private VistaResponsablesExamen vistaResponsablesExamen;
+	private VistaAula vistaAula;
+	
 
+	
+	
 	public void setVistaHome(VistaHome vistaHome) {
 		this.vistaHome = vistaHome;
 	}
-
-	public void setVistaSedes(VistaSedes vistaSedes) {
-		this.vistaSedes = vistaSedes;
-	}
-
-	public void setVistaAsignaturas(VistaAsignaturas vistaAsignaturas) {
-		this.vistaAsignaturas = vistaAsignaturas;
-	}
-
 	public void setVistaAlumnos(VistaAlumnos vistaAlumnos){
 		this.vistaAlumnos = vistaAlumnos;
 	}
-
-	public void setVistaResponsables(VistaResponsablesSede vistaResponsables) {
-		this.vistaResponsables = vistaResponsables;
-
-	}
-	public void setVistaResponsablesExamen(VistaResponsablesExamen vistaResponsablesExamen) {
-		this.vistaResponsablesExamen = vistaResponsablesExamen;
-
+	public void setVistaAsignaturas(VistaAsignaturas vistaAsignaturas) {
+		this.vistaAsignaturas = vistaAsignaturas;
 	}
 	public void setVistaExamen(VistaExamen vistaExamen) {
 		this.vistaExamen = vistaExamen;
 
 	}
-	public void setVistaAula(VistaAula vistaAula){
-		this.vistaAula = vistaAula;
-	}
 	public void setVistaInstituto(VistaInstituto vistaInstituto) {
 		this.vistaInstituto = vistaInstituto;
 	}
+	public void setVistaResponsables(VistaResponsablesSede vistaResponsables) {
+		this.vistaResponsables = vistaResponsables;
+
+	}	
+	public void setVistaSedes(VistaSedes vistaSedes) {
+		this.vistaSedes = vistaSedes;
+	}
+		
+
+	public void setVistaAula(VistaAula vistaAula){
+		this.vistaAula = vistaAula;
+	}
+	public void setVistaResponsablesExamen(VistaResponsablesExamen vistaResponsablesExamen) {
+		this.vistaResponsablesExamen = vistaResponsablesExamen;
+	}
+	
+	
+	
 	
 
 
 	public void mostrarHome() {
 		setVisibleFalse();
 		vistaHome.setVisible(true);
-
 	}
 
-	public void mostrarExamenes() {
-		setVisibleFalse();
-		vistaExamen.setVisible(true);
-
-	}
-
+	
 	public void mostrarAlumnos() {
 		setVisibleFalse();
 		vistaAlumnos.setVisible(true);
@@ -75,52 +73,59 @@ public class Controlador{
 		vistaAsignaturas.setVisible(true);
 	}
 
-	public void mostrarSedes() {
+		
+	public void mostrarExamenes() {
 		setVisibleFalse();
-		vistaSedes.setVisible(true);
-	}
-
-	public void mostrarAulas() {
-		setVisibleFalse();
-		vistaAula.setVisible(true);
-	}
-
-	public void mostrarResponsables() {
-		setVisibleFalse();
-		vistaResponsables.setVisible(true);
+		vistaExamen.setVisible(true);
 	}
 
 	public void mostrarInstitutos() {
 		setVisibleFalse();
 		vistaInstituto.setVisible(true);
 	}
-
-	public void mostrarGenerar() {
+	
+	public void mostrarResponsables() {
 		setVisibleFalse();
-		//vistaGenerar.setVisible(true);
+		vistaResponsables.setVisible(true);
+	}
+	
+	public void mostrarSedes() {
+		setVisibleFalse();
+		vistaSedes.setVisible(true);
+	}
+
+	
+	
+	public void mostrarAulas() {
+		setVisibleFalse();
+		vistaAula.setVisible(true);
 	}
 
 	public void mostrarResponsablesExamenes() {
 		setVisibleFalse();
 		vistaResponsablesExamen.setVisible(true);
-
 	}
 
+	
+	public void mostrarGenerar() {
+		setVisibleFalse();
+		//vistaGenerar.setVisible(true);
+	}
+
+	
 	public void setVisibleFalse(){
 		vistaHome.setVisible(false);
-		
 		vistaAlumnos.setVisible(false);
 		vistaAsignaturas.setVisible(false);
-		vistaSedes.setVisible(false);
-		
-		vistaResponsables.setVisible(false);
+		vistaExamen.setVisible(false);
 		vistaInstituto.setVisible(false);
-		//vistaGenerar.setVisible(false);
-
+		vistaSedes.setVisible(false);
+		vistaResponsables.setVisible(false);
+		
 		vistaAula.setVisible(false);
 		vistaResponsablesExamen.setVisible(false);
 
-
+		//vistaGenerar.setVisible(false);
 	
 	}
 
