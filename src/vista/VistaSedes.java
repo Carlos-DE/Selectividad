927aac45d86d4bcd93e80396e19b109aba125da1
sergioM <sermorepe@gmail.com>
2022-12-20 14:09:52 +0100

    funcionan todos los botones de navegacion entre ventanas

--- src/vista/VistaSedes.java
+++ src/vista/VistaSedes.java
@@ -13,15 +13,17 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import javax.swing.border.TitledBorder;
 
 public class VistaSedes extends JFrame implements ActionListener{
-
+	private JButton bCargarDatos, bBorrarDatos;
+	private JButton bVicerrector;
+	private JButton bGestorSede;
 	private JButton bHome;
-	private JButton bSedes, bCargarDatos, bBorrarDatos;
-	private JButton bAlumnos;
-	private JButton bAsignaturas;
+	private JButton bExamenes;
+	private JButton bAlumnos,bAsignaturas,bSedes,bResponsablesSedes;
+
 	private JButton bAulas;
-	private JButton bResponsables;
 	private JButton bInstitutos;
 	private JButton bGenerar;
 	private JTextField fieldDireccion;
@@ -64,53 +66,86 @@ public class VistaSedes extends JFrame implements ActionListener{
 		getContentPane().add(maingrid, BorderLayout.CENTER);
 		GridBagLayout gbl_maingrid = new GridBagLayout();
 		gbl_maingrid.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_maingrid.rowHeights = new int[]{40, 30, 30, 200, 15, 30, 45, 60, 15, 0};
-		gbl_maingrid.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_maingrid.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_maingrid.rowHeights = new int[]{40, 0, 0, 0, 0, 30, 30, 200, -104, 30, 45, 60, 15, 0};
+		gbl_maingrid.columnWeights = new double[]{1.0, 1.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_maingrid.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
 		maingrid.setLayout(gbl_maingrid);
 		
-		
-		
 		Panel menu = new Panel();
-		GridBagConstraints gbc_menu = new GridBagConstraints();
-		gbc_menu.fill = GridBagConstraints.BOTH;
-		gbc_menu.gridwidth = 6;
-		gbc_menu.insets = new Insets(0, 0, 5, 0);
-		gbc_menu.gridx = 0;
-		gbc_menu.gridy = 0;
-		maingrid.add(menu, gbc_menu);
 		menu.setFont(new Font("Dialog", Font.PLAIN, 14));
 		menu.setBackground(new Color(0, 64, 128));
+		GridBagConstraints gbc_menu = new GridBagConstraints();
+		gbc_menu.fill = GridBagConstraints.BOTH;
+		gbc_menu.gridwidth = 5;
+		gbc_menu.insets = new Insets(0, 0, 5, 5);
+		gbc_menu.gridx = 0;
+		gbc_menu.gridy = 2;
+		maingrid.add(menu, gbc_menu);
 		
 		bHome = new JButton("Home");
-		bHome.addActionListener(this);
 		menu.add(bHome);
+		bHome.addActionListener(this);
+
+		bVicerrector = new JButton("VICERRECTOR");
+		menu.add(bVicerrector);
+		bVicerrector.addActionListener(this);
+
+		bGestorSede = new JButton("GESTOR SEDE");
+		menu.add(bGestorSede);
+		bGestorSede.addActionListener(this);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new TitledBorder(null, "VICERRECTOR", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.gridwidth = 6;
+		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.gridx = 0;
+		gbc_panel_2.gridy = 3;
+		maingrid.add(panel_2, gbc_panel_2);
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_2.rowHeights = new int[]{0, 0};
+		gbl_panel_2.columnWeights = new double[]{1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
+		
+		Panel menu_1 = new Panel();
+		menu_1.setFont(new Font("Dialog", Font.PLAIN, 14));
+		menu_1.setBackground(new Color(0, 64, 128));
+		GridBagConstraints gbc_menu_1 = new GridBagConstraints();
+		gbc_menu_1.insets = new Insets(0, 0, 0, 5);
+		gbc_menu_1.fill = GridBagConstraints.BOTH;
+		gbc_menu_1.gridwidth = 4;
+		gbc_menu_1.gridx = 0;
+		gbc_menu_1.gridy = 0;
+		panel_2.add(menu_1, gbc_menu_1);
 		
 		bAlumnos = new JButton("Alumnos");
+		menu_1.add(bAlumnos);
 		bAlumnos.addActionListener(this);
-		menu.add(bAlumnos);
+
+		bAsignaturas = new JButton("Asignaturas");
+		menu_1.add(bAsignaturas);
+		bAsignaturas.addActionListener(this);
+
+		bExamenes = new JButton("Examenes");
+		menu_1.add(bExamenes);
+		bExamenes.addActionListener(this);
+
+		bInstitutos = new JButton("Institutos");
+		menu_1.add(bInstitutos);
+		bInstitutos.addActionListener(this);
+		
+		bResponsablesSedes = new JButton("Responsables Sedes");
+		menu_1.add(bResponsablesSedes);
+		bResponsablesSedes.addActionListener(this);
 		
 		bSedes = new JButton("Sedes");
+		menu_1.add(bSedes);
 		bSedes.addActionListener(this);
-		menu.add(bSedes);
 		
-		bAsignaturas = new JButton("Asignaturas");
-		bAsignaturas.addActionListener(this);
-		menu.add(bAsignaturas);
-		
-		
-		
-//		bAulas = new JButton("Aulas");
-//		bAulas.addActionListener(this);
-//		menu.add(bAulas);
-		
-		bResponsables = new JButton("Responsables");
-		bResponsables.addActionListener(this);
-		menu.add(bResponsables);
-		
-//		bInstitutos = new JButton("Institutos");
-//		bInstitutos.addActionListener(this);
-//		menu.add(bInstitutos);
+
 		
 //		bGenerar = new JButton("Generar");
 //		bGenerar.addActionListener(this);
@@ -122,18 +157,16 @@ public class VistaSedes extends JFrame implements ActionListener{
 		
 		JLabel lblNombreSede = new JLabel("Nombre Sede");
 		GridBagConstraints gbc_lblNombreSede = new GridBagConstraints();
-		gbc_lblNombreSede.fill = GridBagConstraints.BOTH;
 		gbc_lblNombreSede.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNombreSede.gridx = 3;
-		gbc_lblNombreSede.gridy = 1;
+		gbc_lblNombreSede.gridy = 5;
 		maingrid.add(lblNombreSede, gbc_lblNombreSede);
 		
 		JLabel lblDireccion = new JLabel("Direccion");
 		GridBagConstraints gbc_lblDireccion = new GridBagConstraints();
-		gbc_lblDireccion.fill = GridBagConstraints.BOTH;
 		gbc_lblDireccion.insets = new Insets(0, 0, 5, 5);
 		gbc_lblDireccion.gridx = 4;
-		gbc_lblDireccion.gridy = 1;
+		gbc_lblDireccion.gridy = 5;
 		maingrid.add(lblDireccion, gbc_lblDireccion);
 		
 		
@@ -144,44 +177,41 @@ public class VistaSedes extends JFrame implements ActionListener{
 		GridBagConstraints gbc_bCargarDatos = new GridBagConstraints();
 		gbc_bCargarDatos.insets = new Insets(0, 0, 5, 5);
 		gbc_bCargarDatos.gridx = 1;
-		gbc_bCargarDatos.gridy = 2;
+		gbc_bCargarDatos.gridy = 6;
 		bCargarDatos.addActionListener(this);
 		maingrid.add(bCargarDatos, gbc_bCargarDatos);
 		
 		//List listNombre = new List();
 		listaSedes = new JList<String>(listModel);
 		GridBagConstraints gbc_listNombre = new GridBagConstraints();
-		gbc_listNombre.fill = GridBagConstraints.BOTH;
 		gbc_listNombre.gridheight = 2;
 		gbc_listNombre.insets = new Insets(0, 0, 5, 5);
 		gbc_listNombre.gridx = 3;
-		gbc_listNombre.gridy = 2;
+		gbc_listNombre.gridy = 6;
 		maingrid.add(listaSedes, gbc_listNombre);
 		
 		List listDireccion = new List();
 		GridBagConstraints gbc_listDireccion = new GridBagConstraints();
-		gbc_listDireccion.fill = GridBagConstraints.BOTH;
 		gbc_listDireccion.gridheight = 2;
 		gbc_listDireccion.insets = new Insets(0, 0, 5, 5);
 		gbc_listDireccion.gridx = 4;
-		gbc_listDireccion.gridy = 2;
+		gbc_listDireccion.gridy = 6;
 		maingrid.add(listDireccion, gbc_listDireccion);
 		
 		bBorrarDatos = new JButton("BorrarDatos");
 		GridBagConstraints gbc_bBorrarDatos = new GridBagConstraints();
-		gbc_bBorrarDatos.anchor = GridBagConstraints.NORTH;
 		gbc_bBorrarDatos.insets = new Insets(0, 0, 5, 5);
 		gbc_bBorrarDatos.gridx = 1;
-		gbc_bBorrarDatos.gridy = 3;
+		gbc_bBorrarDatos.gridy = 7;
 		bBorrarDatos.addActionListener(this);
 		maingrid.add(bBorrarDatos, gbc_bBorrarDatos);
 		
 		table = new JTable();
 		GridBagConstraints gbc_table = new GridBagConstraints();
 		gbc_table.insets = new Insets(0, 0, 5, 5);
-		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.fill = GridBagConstraints.VERTICAL;
 		gbc_table.gridx = 3;
-		gbc_table.gridy = 4;
+		gbc_table.gridy = 8;
 		maingrid.add(table, gbc_table);
 		
 		
@@ -192,18 +222,16 @@ public class VistaSedes extends JFrame implements ActionListener{
 		fieldNombre.setColumns(4);
 		GridBagConstraints gbc_fieldNombre = new GridBagConstraints();
 		gbc_fieldNombre.insets = new Insets(1, 1, 5, 5);
-		gbc_fieldNombre.fill = GridBagConstraints.BOTH;
 		gbc_fieldNombre.gridx = 3;
-		gbc_fieldNombre.gridy = 5;
+		gbc_fieldNombre.gridy = 9;
 		
 		maingrid.add(fieldNombre, gbc_fieldNombre);
 		
 		fieldDireccion = new JTextField();
 		GridBagConstraints gbc_fieldDireccion = new GridBagConstraints();
 		gbc_fieldDireccion.insets = new Insets(0, 0, 5, 5);
-		gbc_fieldDireccion.fill = GridBagConstraints.BOTH;
 		gbc_fieldDireccion.gridx = 4;
-		gbc_fieldDireccion.gridy = 5;
+		gbc_fieldDireccion.gridy = 9;
 		maingrid.add(fieldDireccion, gbc_fieldDireccion);
 		fieldDireccion.setColumns(10);
 		
@@ -213,11 +241,10 @@ public class VistaSedes extends JFrame implements ActionListener{
 		
 		JPanel panelCRUD = new JPanel();
 		GridBagConstraints gbc_panelCRUD = new GridBagConstraints();
-		gbc_panelCRUD.anchor = GridBagConstraints.EAST;
 		gbc_panelCRUD.gridwidth = 2;
 		gbc_panelCRUD.insets = new Insets(0, 0, 5, 5);
 		gbc_panelCRUD.gridx = 3;
-		gbc_panelCRUD.gridy = 6;
+		gbc_panelCRUD.gridy = 10;
 		maingrid.add(panelCRUD, gbc_panelCRUD);
 		
 		JButton bEditarSede = new JButton("EditarSede");
@@ -235,52 +262,41 @@ public class VistaSedes extends JFrame implements ActionListener{
 		
 		JButton bDescargarLog = new JButton("DescargarLog");
 		GridBagConstraints gbc_bDescargarLog = new GridBagConstraints();
-		gbc_bDescargarLog.anchor = GridBagConstraints.NORTH;
 		gbc_bDescargarLog.insets = new Insets(0, 0, 5, 5);
 		gbc_bDescargarLog.gridx = 1;
-		gbc_bDescargarLog.gridy = 7;
+		gbc_bDescargarLog.gridy = 11;
 		maingrid.add(bDescargarLog, gbc_bDescargarLog);
 		
 		TextArea logText = new TextArea();
 		GridBagConstraints gbc_logText = new GridBagConstraints();
-		gbc_logText.anchor = GridBagConstraints.NORTH;
-		gbc_logText.fill = GridBagConstraints.HORIZONTAL;
 		gbc_logText.gridwidth = 2;
 		gbc_logText.insets = new Insets(0, 0, 5, 5);
 		gbc_logText.gridx = 3;
-		gbc_logText.gridy = 7;
+		gbc_logText.gridy = 11;
 		maingrid.add(logText, gbc_logText);
 		logText.setEditable(false);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==bHome) {
+		if(e.getSource()==bHome){
 			controlador.mostrarHome();
-		} else if (e.getSource()==bAlumnos) {
+		}else if(e.getSource()==bVicerrector){
 			controlador.mostrarAlumnos();
-		} else if (e.getSource()==bAsignaturas) {
-			controlador.mostrarAsignaturas();
-		} else if (e.getSource()==bSedes) {
-			controlador.mostrarSedes();
-		} else if (e.getSource()==bAulas) {
+		}else if(e.getSource()==bGestorSede){
 			controlador.mostrarAulas();
-		} else if (e.getSource()==bResponsables) {
-			controlador.mostrarResponsables();
-		} else if (e.getSource()==bInstitutos) {
+		}else if (e.getSource()==bAlumnos){
+			controlador.mostrarAlumnos();
+		}else if (e.getSource()==bAsignaturas){
+			controlador.mostrarAsignaturas();
+		}else if (e.getSource()==bExamenes){
+			controlador.mostrarExamenes();
+		}else if (e.getSource()==bInstitutos){
 			controlador.mostrarInstitutos();
-		} else if (e.getSource()==bGenerar) {
-			controlador.mostrarGenerar();
-		} else if(e.getSource()==bCargarDatos) {
-			System.out.println("llego al boton");
-			try {
-				controladorSede.abrirArchivo();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-			System.out.println("llego al boton");
-		} else if (e.getSource()==bBorrarDatos) {
-			controladorSede.borrarDatos();
+		}else if (e.getSource()==bResponsablesSedes){
+			controlador.mostrarResponsables();
+		}else if (e.getSource()==bSedes){
+			controlador.mostrarSedes();
 		}
 	}
 	
